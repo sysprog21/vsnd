d085e6797fd7b48f52c952f50fea55a791789f41
Cuda-Chen <clh960524@gmail.com>
2024-03-18 21:21:21 +0800

    Add PulseAudio-enabled environment running guide

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -11,7 +11,7 @@ sudo insmod vsnd.ko out_fifo_name=/tmp/audio.pcm
 
 tee out.pcm < /tmp/audio.pcm >/dev/null &
 sleep 1
-aplay CantinaBand3.wav
+aplay -D plughw:CARD=vsnd,DEV=0 CantinaBand3.wav
 sudo rmmod vsnd
 
 ffmpeg -f s16le -ar 22.05k -ac 1 -loglevel 8 -i out.pcm out.wav
